51611d93dc6e70f28604df49871b3995c197eed1
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2023-10-03 00:53:35 +0200

    Update Prueba1.java

--- Prueba1.java
+++ Prueba1.java
@@ -1,3 +1,16 @@
+import java.util.*;
+
 public class Prueba1 {
-    
+    public static void main(String[] args){
+        airline airline1 = new airline("SafeNSlow");
+        airline airline2 = new airline("CheapNFast");
+        
+        LinkedList<airline> airlineList = new LinkedList<airline>();
+        airlineList.add(airline1);
+        airlineList.add(airline2);
+
+        for( airline airline : airlineList ) {
+            airline.printName();
+        }
+     }
 }
