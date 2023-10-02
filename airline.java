dade2d00797fe5c6cf91d5ce4ef4563187960e6f
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2023-10-02 15:55:08 +0200

    Commit 1

---
+++ airline.java
@@ -0,0 +1,10 @@
+public class airline{
+    private String name;
+
+    public airline(String initName){
+        name = initName; 
+    }
+    public void printName(){
+        System.out.println(name);
+    }
+}
