cd1441064075caf7c0aed81cf6dc918ed5964583
Bieldogi <131766948+Bieldogi@users.noreply.github.com>
2023-11-02 14:49:41 +0100

    intent de cup

--- Lab3/Cup.java
+++ Lab3/Cup.java
@@ -1,3 +1,47 @@
-public class Cup {
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class Cup extends Competition {
+    private LinkedList<Team> tr;
+    private LinkedList<Match> mr;
+
+    public Cup(String name, Country country, L_Gender gender){
+        super(name, country, gender);
+        this.tr = new LinkedList<Team>();
+        this.mr = new LinkedList<Match>();   
+    }
+
+    public int getNumTeams(){
+        int numEquipos = 0;
+        Iterator<Team> iterator = tr.iterator(); 
+        
+        while (iterator.hasNext()) {
+            Team equipo = iterator.next();
+            numEquipos += 1;
+        }
+        return numEquipos;    
+
+    }
+    public int getRounds(){
     
+        int rondas = (int) Math.ceil(Math.log(getNumTeams()) / Math.log(2));
+        return rondas;
+
+
+    }
+    public void generateMatches(){
+        int numMatches = 2^getRounds();
+        int Matches2Teams = (getNumTeams()-(numMatches - getNumTeams()))/2;
+        for
+    }
+
+    public void simulateMatches(){
+        //FER
+    }
+
+    public void printBracked(){
+        //FER
+    }
+
+
 }
