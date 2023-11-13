34bb8e6e7db310a84304a26843b747f1520b1277
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2023-11-13 14:09:42 +0100

    Update Cup.java

--- Lab3/Cup.java
+++ Lab3/Cup.java
@@ -1,68 +1,76 @@
 import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Cup extends Competition {
-    private List<Team> tr;
-    private List<CupMatch> mr;
+    private Team[][] tr;
+    private CupMatch[][] mr;
 
     public Cup(String name, Country country, L_Gender gender, boolean clubs) {
         super(name, country, gender, clubs);
-        this.mr = new ArrayList<CupMatch>();
-        this.tr = new ArrayList<Team>();
+        this.tr = new Team[10][]; // Asume un máximo de 10 rondas
+        this.mr = new CupMatch[10][];
     }
 
     public void TeamstoCompetition() {
-        tr.addAll(teams);
+        tr[0] = teams.toArray(new Team[0]);
     }
 
     public void generateMatches() {
-        while (tr.size()>1) {
-            Collections.shuffle(tr);
-            int numTeams = tr.size();
+        System.out.println("\nLos partidos de "+this.name+":");
+        for (int round = 0; round < tr.length && tr[round].length > 1; round++) {
+            System.out.println("\nRonda " + (round+1) + " donde forman parte:\n" );
+            for(Team t: tr[round]){
+                System.out.println("-"+t.name);
+            }
+
+            Collections.shuffle(Arrays.asList(tr[round]));
+            int numTeams = tr[round].length;
             Team t = null;
             
             if (numTeams % 2 != 0) {
-                t = tr.remove(numTeams - 1);
+                t = tr[round][numTeams - 1];
+                tr[round] = Arrays.copyOf(tr[round], numTeams - 1); // Elimina el último equipo
             }
     
-            for (int i = 0; i < tr.size(); i += 2) { 
-                Team team1 = tr.get(i);
-                Team team2 = tr.get(i + 1);
+            mr[round] = new CupMatch[tr[round].length / 2];
+            for (int i = 0; i < tr[round].length; i += 2) { 
+                Team team1 = tr[round][i];
+                Team team2 = tr[round][i + 1];
                 CupMatch cupmatch = new CupMatch(team1, team2);
-                mr.add(cupmatch);
+                mr[round][i / 2] = cupmatch;
             }
     
-            List<Team> winnList = new ArrayList<>();
-
-            for (CupMatch m : mr) {
+            tr[round+1] = new Team[mr[round].length + (t != null ? 1 : 0)];
+            for (int i = 0; i < mr[round].length; i++) {
+                CupMatch m = mr[round][i];
                 m.simulateMatch();
                 m.printmatch(this);
                 m.getHomeTeam().updateStats(m);
                 m.getAwayTeam().updateStats(m);
                 if (m.getHomeGoals() < m.getAwayGoals()) {
-                    winnList.add(m.getAwayTeam());
+                    tr[round+1][i] = m.getAwayTeam();
                 } else {
-                    winnList.add(m.getHomeTeam());
+                    tr[round+1][i] = m.getHomeTeam();
                 }
             }
             
             if (t != null){
-                winnList.add(t);
+                tr[round+1][tr[round+1].length - 1] = t;
             }
-            tr = winnList;
-            mr.clear();
-            
         }
-        
-        
     }
     
     @Override
     public void simulateMatches() {
-        if (!tr.isEmpty()) {
-            System.out.println("El ganador de " + this.name + " es:\n" + tr.get(0).name);
+        int lastRound = 0;
+        for (int i = 0; i < tr.length; i++) {
+            if (tr[i] != null && tr[i].length > 0) {
+                lastRound = i;
+            }
+        }
+        if (tr[lastRound] != null && tr[lastRound].length > 0) {
+            System.out.println("El ganador de " + this.name + " es:\n" + tr[lastRound][0].name);
         } else {
             System.out.println("No se han simulado partidos en " + this.name);
         }
